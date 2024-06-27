e577a87112e358ac73277dcaee5917913e6efca0
bramirez246 <bramirez246@gmail.com>
2024-06-27 12:47:41 -0700

    added hero component

--- Hero.UI/src/app/app.routes.ts
+++ Hero.UI/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HeroComponent,
+        title: 'Hero Page'
+    }
+];
