19ee4d264d766b44b9ee99f127e0b4e33cf84cb3
bramirez246 <bramirez246@gmail.com>
2024-07-02 12:22:11 -0700

    added grid for displaying heroes

--- Hero.UI/src/app/hero/hero.component.ts
+++ Hero.UI/src/app/hero/hero.component.ts
@@ -1,22 +1,70 @@
 import { Component } from '@angular/core';
 import { HeroInfo } from '../interfaces/hero-info';
 import { HeroInfoComponent } from '../hero-info/hero-info.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeroInfoComponent],
+  imports: [HeroInfoComponent, NgFor],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-  hero: HeroInfo = {
-    id: 1,
-    name: "Deadpool",
-    firstName: "Wade",
-    lastName: "Wilson",
-    birthplace: "Canada",
-    universe: "Marvel",
-    isFavorite: true
-  };
+  heroList: HeroInfo[] = [
+    {
+      id: 0,
+      name: "Deadpool",
+      firstName: "Wade",
+      lastName: "Wilson",
+      birthplace: "Canada",
+      universe: "Marvel",
+      isFavorite: true
+    },
+    {
+      id: 1,
+      name: "God of Thunder",
+      firstName: "Thor",
+      lastName: "Odinson",
+      birthplace: "Asgard",
+      universe: "Marvel",
+      isFavorite: true
+    },
+    {
+      id: 2,
+      name: "Nightwing",
+      firstName: "Rirchard",
+      lastName: "Grayson",
+      birthplace: "Some circus or something",
+      universe: "DC",
+      isFavorite: false
+    },
+    {
+      id: 3,
+      name: "Batman",
+      firstName: "Bruce",
+      lastName: "Wayne",
+      birthplace: "Gotham",
+      universe: "DC",
+      isFavorite: true
+    },
+    {
+      id: 4,
+      name: "Superman",
+      firstName: "Kal-El",
+      lastName: "Kent",
+      birthplace: "Krypton",
+      universe: "DC",
+      isFavorite: true
+    },
+    {
+      id: 5,
+      name: "Ghost Rider",
+      firstName: "Johnny",
+      lastName: "Blaze",
+      birthplace: "Wukegan, Illinois",
+      universe: "Marvel",
+      isFavorite: true
+    },
+  ];
 }
