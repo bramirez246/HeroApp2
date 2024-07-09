0704b504a6530ed9cb4f1003bcd8306cd3b3cd20
bramirez246 <bramirez246@gmail.com>
2024-07-09 12:55:49 -0700

    generated HeroAPI .NET project

---
+++ HeroAPI/HeroAPI/Controllers/HeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Hero>>> GetAllHeroes()
+        {
+            return new List<Hero> 
+            { 
+                new Hero 
+                {
+                    Name = "Deadpool",
+                    FirstName = "Wade",
+                    LastName = "Wilson",
+                    Birthplace = "Canada",
+                    Universe = "Marvel",
+                    IsFavorite = true,
+                } 
+            };
+        }
+    }
+}
---
+++ HeroAPI/HeroAPI/Hero.cs
@@ -0,0 +1,13 @@
+﻿namespace HeroAPI
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Birthplace {  get; set; } = string.Empty;
+        public string Universe {  get; set; } = string.Empty;
+        public bool IsFavorite { get; set; }
+    }
+}
